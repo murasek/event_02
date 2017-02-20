0c907612b9f4caee3f7778e96e3a871727044271
koichimurase <k01_mura@yahoo.co.jp>
2017-02-20 14:04:23 +0000

    20172020

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20170115022338) do
   end
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "title"
-    t.text     "email"
+   
+    
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
