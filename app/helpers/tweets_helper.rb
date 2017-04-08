1387d910caf84f39dd179ab0967332b976fa7007
koichimurase <k01_mura@yahoo.co.jp>
2017-04-08 10:32:11 +0000

    twitterlikeapp

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,6 +1,6 @@
 module TweetsHelper
    def choose_new_or_edit
-      if action_name == 'confirm'
+      if action_name == 'new' ||action_name == 'confirm'
       confirm_tweets_path
       elsif action_name == 'edit'
       tweet_path
