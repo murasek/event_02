613b4f0d98f2357ff479f2ad0e3fb1cea0edd3c0
k-term <k01_mura@yahoo.co.jp>
2018-01-22 18:55:41 +0900

    20180122001

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,22 +1,23 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!
  before_action :set_blog, only: [:show, :edit, :update, :destroy]
-  
+
   def index
+    
     @blogs = Blog.all
    respond_to do |format|
     format.html
     format.js
    end
   end
- 
+
   # showアククションを定義します。入力フォームと一覧を表示するためインスタンスを2つ生成します。
   def show
     @comment = @blog.comments.build
     @comments = @blog.comments
     Notification.find(params[:notification_id]).update(read: true) if params[:notification_id]
   end
-  
+
 
   def new
     if params[:back]
@@ -40,31 +41,31 @@ class BlogsController < ApplicationController
  end
 
   def edit
-    
+
   end
-    
+
   def update
-   
+
     if @blog.update(blogs_params)
     redirect_to blogs_path
     else
-     render 'edit' 
+     render 'edit'
     end
   end
-  
+
   def destroy
-    
+
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
    @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
-  
-  
+
+
+
    private
     def blogs_params
       params.require(:blog).permit(:title, :content)
@@ -72,5 +73,4 @@ class BlogsController < ApplicationController
     def set_blog
       @blog = Blog.find(params[:id])
     end
-end 
-    
+end
