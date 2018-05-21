0c036211facbedba2efac5cf61e21a128a15c95b
k-term <k01_mura@yahoo.co.jp>
2018-05-21 22:10:16 +0900

    20180520commentcontr修正001

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
   def create
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
-    
+
   @comment = current_user.comments.build(comment_params)
   @blog = @comment.blog
   @notification = @comment.notifications.build(user_id: @blog.user.id )
-    
-    
+
+
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-         
+
         # JS形式でレスポンスを返します。
         format.js { render :index }
-        
-       
+
+
         unless @comment.blog.user_id == current_user.id
           Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
             message: 'あなたの作成したブログにコメントが付きました'
@@ -29,6 +29,23 @@ class CommentsController < ApplicationController
       end
     end
   end
+
+
+
+  def edit
+   @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+    redirect_to blog_path(@comment.blog), notice:"コメントを編集しました！"
+      else
+        render 'edit' if @comment.invalid?
+      end
+    end
+  end
+
 
 
  def destroy 
