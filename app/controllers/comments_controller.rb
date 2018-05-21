6ec38dd9adfcc12996f32032a31c01026b1ec00a
k-term <k01_mura@yahoo.co.jp>
2018-05-21 23:14:57 +0900

    20180520commentcontr修正003

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,12 @@
 class CommentsController < ApplicationController
-  def create
-    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
-
-  @comment = current_user.comments.build(comment_params)
-  @blog = @comment.blog
-  @notification = @comment.notifications.build(user_id: @blog.user.id )
-
-
-    # クライアント要求に応じてフォーマットを変更
+def create
+    @comment = current_user.comments.build(comment_params)
+    @blog = @comment.blog
+    @notification = @comment.notifications.build(user_id: @blog.user.id )
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-
-        # JS形式でレスポンスを返します。
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました' }
         format.js { render :index }
-
-
         unless @comment.blog.user_id == current_user.id
           Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
             message: 'あなたの作成したブログにコメントが付きました'
@@ -27,40 +18,30 @@ class CommentsController < ApplicationController
       else
         format.html { render :new }
       end
+end
+
+def edit
+ @comment = Comment.find(params[:id])
+end
+
+def update
+  @comment = Comment.find(params[:id])
+  if @comment.update(comment_params)
+  redirect_to blog_path(@comment.blog), notice:"コメントを編集しました！"
+    else
+      render 'edit' if @comment.invalid?
     end
   end
+end
 
-
-
-  def edit
-   @comment = Comment.find(params[:id])
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-    if @comment.update(comment_params)
-    redirect_to blog_path(@comment.blog), notice:"コメントを編集しました！"
-      else
-        render 'edit' if @comment.invalid?
-      end
-    end
-  end
-
-
-
- def destroy
+def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
     render :index
- end
-
-
-
-
+end
 
   private
-    # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
     end
-end
+  end
