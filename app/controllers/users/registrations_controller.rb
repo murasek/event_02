e0ae4cb548b225700be6bce67dfd70153bb2c556
k-term <k01_mura@yahoo.co.jp>
2018-05-23 00:36:11 +0900

    20180522registcont修正003

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def build_resource(hash=nil)
+#  def build_resource(hash=nil)
+#    hash[:uid] = User.create_unique_string
+#    super
+#  end
+
+  def build_resource(hash = {})
     hash[:uid] = User.create_unique_string
     super
   end
-  def new
-    build_resource
-      yield resource if block_given?
-      respond_with resource
-  end
+
 end
