057105633cc2f5f4080250e7231106642309662a
k-term <k01_mura@yahoo.co.jp>
2018-05-26 19:32:09 +0900

    20180526001

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
   protect_from_forgery with: :exception
 
   # before_actionで下で定義したメソッドを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   #変数PERMISSIBLE_ATTRIBUTESに配列[:name]を代入
-    PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
+  #PERMISSIBLE_ATTRIBUTES = %i(name)
 
-    protected
+  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
+  protected
 
     #deviseのストロングパラメーターにカラム追加するメソッドを定義
     def configure_permitted_parameters
@@ -18,8 +18,15 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
     end
 
-　　before_action :current_notifications, if: :signed_in? 
-    def current_notifications
-  　@notifications_count = Notification.where(user_id: current_user.id).where(read:false).count
-    end
+  add_flash_types :success, :info, :warning, :danger
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  # DIVE19-2
+  before_action :current_notifications, if: :signed_in?
+  def current_notifications
+   @notifications_count = Notification.where(user_id: current_user.id).where(read: false).count
+  end
+
 end
