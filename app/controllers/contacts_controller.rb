0dafccf42d463191e22d700776976bd4196be56f
k-term <k01_mura@yahoo.co.jp>
2018-05-27 15:11:18 +0900

    20180527001

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,33 +1,33 @@
 class ContactsController < ApplicationController
  before_action :authenticate_user!
   def new
-   
+
       @contact = Contact.new
-  
+
   end
-  
+
   def create
     @contact = Contact.new(contacts_params)
      if params[:back]
        render action: 'new'
-    
-    else if 
+
+    else if
       @contact.save
       redirect_to new_contact_path,notice:"お問い合わせありがとうございます！"
-      
+          NoticeMailer.sendmail_contact(@contact).deliver
       else
-       render action: 'new' 
+       render action: 'new'
       end
      end
-  end 
-    
-    
+  end
+
+
 
   def confirm
    @contact = Contact.new(contacts_params)
     render :new if @contact.invalid?
   end
-  
+
 
   private
     def contacts_params
