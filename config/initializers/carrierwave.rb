ee6dcc57cd71e457064bf0f294d884038763e9e3
k-term <k01_mura@yahoo.co.jp>
2018-06-04 22:37:18 +0900

    20180604001

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       config.fog_directory = 'murasek-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/murasek-production'
     when 'development'
-      config.fog_directory = 'murasek-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/murasek-development'
+      config.fog_directory = 'murasek-develpment'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/murasek-develpment'
   end
 end
