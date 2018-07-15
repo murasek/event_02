30441619a565cda0a315e37a72b24ff6d7c11a02
k-term <k01_mura@yahoo.co.jp>
2018-07-16 00:09:14 +0900

    init unicorn_fakebook

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,16 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'fakebook'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/murasek/achieve.git'
+set :repo_url, 'https://github.com/murasek/fakebook.git'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/fakebook'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '52.198.143.78', user: 'app', roles: %w{app db web}
+server '52.194.63.107', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/k01_mura/.ssh/id_rsa'
