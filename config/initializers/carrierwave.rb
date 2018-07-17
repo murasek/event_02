d475d38c48f90f67bae6cb3b50c17f88522c5c9e
k-term <k01_mura@yahoo.co.jp>
2018-07-17 22:25:23 +0900

    20180717002

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = 'fakebook'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fakebook'
+      config.fog_directory = 'murasek-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/murasek-production'
     when 'development'
       config.fog_directory = 'murasek-develpment'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/murasek-develpment'
