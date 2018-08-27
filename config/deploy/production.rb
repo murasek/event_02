583a1f809720acef887ca69fc2b2e06d5bd335f5
k-term <k01_mura@yahoo.co.jp>
2018-08-27 21:39:44 +0900

    20180827001

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,17 @@
-server '52.194.63.107', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/k01_mura/.ssh/id_rsa'
+#aws利用じに仕様
+# server '52.194.63.107', user: 'app', roles: %w{app db web}
+# set :ssh_options, keys: '/Users/k01_mura/.ssh/id_rsa'
+
+
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings =
+{
+ user_name: ENV['SENDGRID_USERNAME'],
+ password: ENV['SENDGRID_PASSWORD'],
+ domain: "heroku.com",
+ address: "smtp.sendgrid.net",
+ port: 587,
+ authentication: :plain,
+ enable_starttls_auto: true
+}
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,4 @@
 
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
 
 
 CarrierWave.configure do |config|
