aafa1c965e8fa9aca8e0fb4671dc8b34f578040c
k-term <k01_mura@yahoo.co.jp>
2018-12-08 17:34:51 +0900

    20181208001

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
-  end  
-   
-  
+  end
+
+
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -16,6 +16,18 @@ module EventsHelper
    end
 
 
-  
+   def devise_error_messages2
+     return "" if resource.errors.empty?
+     html = ""
+     # エラーメッセージ用のHTMLを生成
+     messages = resource.errors.full_messages.each do |msg|
+       html += <<-EOF
+         <div class="error_field alert alert-danger" role="alert">
+           <p class="error_msg">#{msg}</p>
+         </div>
+       EOF
+     end
+     html.html_safe
+   end
 
 end
